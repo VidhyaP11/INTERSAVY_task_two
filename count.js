0d72f7d331493413e0f4c40465b069539f1a767c
VidhyaP11 <134631630+VidhyaP11@users.noreply.github.com>
2023-08-28 22:47:58 +0530

    Add files via upload

---
+++ count.js
@@ -0,0 +1,19 @@
+function updateCountdown() {
+    const countdownDate = new Date("2023-12-31T23:59:59").getTime();
+    const now = new Date().getTime();
+    const timeLeft = countdownDate - now;
+  
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  
+    document.getElementById("days").textContent = days.toString().padStart(2, "0");
+    document.getElementById("hours").textContent = hours.toString().padStart(2, "0");
+    document.getElementById("minutes").textContent = minutes.toString().padStart(2, "0");
+    document.getElementById("seconds").textContent = seconds.toString().padStart(2, "0");
+  }
+  
+  updateCountdown();
+  setInterval(updateCountdown, 1000);
+  
